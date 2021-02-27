bc10164905cb0ca9204ee2f6be608ad50837bac1
LostSoul <github@terrorbase.de>
2021-02-27 18:34:02 +0100

    Formal Update

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<!DOCTYPE html>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"
+ "http/www.w3.org/TR/html4/frameset.dtd">
+
+<html lang="de">
   <head>
     <meta charset="utf-8">
     <title>LiquidSoap Controller v2</title>
